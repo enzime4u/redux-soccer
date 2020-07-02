09bd4dc56a41a765f1e2e9a6dd3ff1972f98b444
Liviu Tudor <enzime4u@yahoo.com>
2020-07-02 16:08:04 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import cuid from 'cuid';
 
-function App() {
+function mapStateToProps(state) {
+  return {
+    teams: state.teams,
+    matches: state.matches,
+  };
+}
+
+function App(props) {
+  const [teamName, setTeamName] = useState('');
+  const [teamOne, setTeamOne] = useState('');
+  const [teamTwo, setTeamTwo] = useState('');
+  const [score, setScore] = useState('');
+
+  const addTeam = () => {
+    props.dispatch({
+      type: 'ADD_TEAM',
+      payload: { name: teamName, id: cuid() },
+    });
+    setTeamName('');
+  };
+
+  const removeTeam = (id) => {
+    props.dispatch({ type: 'REMOVE_TEAM', payload: { id: id } });
+  };
+
+  const makeMatch = () => {
+    props.dispatch({
+      type: 'MAKE_MATCH',
+      payload: { teamOne, teamTwo, score },
+    });
+    setTeamOne('');
+    setTeamTwo('');
+    setScore('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <input
+        type='text'
+        value={teamName}
+        onChange={(e) => setTeamName(e.target.value)}
+      />
+      <button onClick={addTeam}>Add Team</button>
+      <ul>
+        {props.teams.map((team) => (
+          <li key={team.id}>
+            {team.name}
+            <button onClick={() => removeTeam(team.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+
+      <div>
+        <select
+          value={teamOne}
+          onChange={(event) => setTeamOne(event.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <option>Select Team 1</option>
+          {props.teams.map((team) => (
+            <option key={team.id} value={team.name}>
+              {team.name}
+            </option>
+          ))}
+        </select>
+        <select
+          value={teamTwo}
+          onChange={(event) => setTeamTwo(event.target.value)}
+        >
+          <option>Select Team 2</option>
+          {props.teams.map((team) => (
+            <option key={team.id} value={team.name}>
+              {team.name}
+            </option>
+          ))}
+        </select>
+        <input
+          type='text'
+          value={score}
+          onChange={(event) => setScore(event.target.value)}
+        />
+        <button onClick={() => makeMatch()}>Make Match</button>
+      </div>
+      <div>
+        <ul>
+          {props.matches.map((match, index) => (
+            <li key={index}>
+              {match.teamOne}-{match.teamTwo} {match.score}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,10 +4,47 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const initialState = {
+  teams: [
+    { id: 123, name: 'Steaua Bucuresti' },
+    { id: 1234, name: 'Dinamo' },
+    { id: 12345, name: 'Farul Constanta' },
+  ],
+  matches: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TEAM': {
+      state.teams = [...state.teams, action.payload];
+      console.log('from add_team', state);
+      return { ...state };
+    }
+    case 'REMOVE_TEAM': {
+      state.teams = [
+        ...state.teams.filter((team) => team.id !== action.payload.id),
+      ];
+      console.log('From Remove Team:', action.id);
+      return { ...state };
+    }
+    case 'MAKE_MATCH': {
+      state.matches = [...state.matches, action.payload];
+      return { ...state };
+    }
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
